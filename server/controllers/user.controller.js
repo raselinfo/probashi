332c6ed75bd9ecc80fc4afc5fc7d8af60c677a7e
Rasel Hossain <rabinarahman@gmail.com>
2022-10-14 22:33:29 +0600

    feat(User Create): User Create

--- server/controllers/user.controller.js
+++ server/controllers/user.controller.js
@@ -1,12 +1,29 @@
-const usersController={
-    createUser(req,res){
+const User = require("../models/user.model")
+const usersController = {
+    async createUser(req, res) {
         console.log(req.body)
-        const {}=req.body
-        res.send("Create user Controller")
+        const { name, fatherName, motherName, address, issueDate } = req.body
+        if (!name || !fatherName || !motherName || !address || !issueDate) {
+            return res.status(400).json({ message: "All fields are required!" })
+        }
+        try {
+            const user = new User({
+                name,
+                fatherName,
+                motherName,
+                address,
+                issueDate
+            })
+            const newUser = await user.save()
+            console.log(newUser)
+            res.status(201).json({ message: "User created successfully!", data: newUser })
+        } catch (err) {
+            res.status(500).json({ message: err.message })
+        }
     },
-    getUsers(req,res){
+    getUsers(req, res) {
         res.send("Get all users")
     }
 }
 
-module.exports=usersController
+module.exports = usersController
