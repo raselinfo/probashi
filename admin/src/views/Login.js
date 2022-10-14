f07ccbd813e478d0372bf8941223ad3e8dc88bc1
Rasel Hossain <rabinarahman@gmail.com>
2022-10-14 11:35:36 +0600

    config(Dockerfile): Admin docker file create

--- admin/src/views/Login.js
+++ admin/src/views/Login.js
@@ -7,6 +7,7 @@ const Login = () => {
     }
     return (
         <div className='container py-5 my-5'>
+            <h2 className="login__heading">আমি প্রবাসীতে স্বাগতম</h2>
             <div className="col-md-6 mx-auto login-form">
                 <form>
                     <input type="email" placeholder="Enter Yor Email" className='form-control form-control-lg mb-3' />
